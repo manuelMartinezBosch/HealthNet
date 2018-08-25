6f75f5905fb903975c979eee5b3ab59ad5ff3e76
Manuel <manuelmmb1@gmail.com>
2018-08-25 18:56:38 +0200

    extract api and dbo

--- src/api/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sql = new mysqli("localhost", "root", "", "nutrition");
-$error_connection = false;
-if(!$sql) {
-	$error_connection = true;
-} else {
-	mysqli_set_charset($sql, "utf8");
-}
-?>
